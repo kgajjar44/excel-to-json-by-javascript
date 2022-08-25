b807939c70e240e54879b597cc960257abf89aea
Krunal Mistri <krunal.mistri@streebo.com>
2022-08-26 00:01:48 +0530

    Changes

--- excel.js
+++ excel.js
@@ -1,30 +1,99 @@
-let selectedFile;
+let selectedFiles;
 console.log(window.XLSX);
-document.getElementById('input').addEventListener("change", (event) => {
-    selectedFile = event.target.files[0];
-})
-
-let data=[{
-    "name":"jayanth",
-    "data":"scd",
-    "abc":"sdef"
-}]
-
-
-document.getElementById('button').addEventListener("click", () => {
-    XLSX.utils.json_to_sheet(data, 'out.xlsx');
-    if(selectedFile){
-        let fileReader = new FileReader();
-        fileReader.readAsBinaryString(selectedFile);
-        fileReader.onload = (event)=>{
-         let data = event.target.result;
-         let workbook = XLSX.read(data,{type:"binary"});
-         console.log(workbook);
-         workbook.SheetNames.forEach(sheet => {
-              let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
-              console.log(rowObject);
-              document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4)
-         });
-        }
-    }
+document.getElementById("input").addEventListener("change", (event) => {
+  selectedFiles = event.target.files;
 });
+
+let data = [
+  {
+    name: "jayanth",
+    data: "scd",
+    abc: "sdef",
+  },
+];
+
+document.getElementById("button").addEventListener("click", () => {
+  XLSX.utils.json_to_sheet(data, "out.xlsx");
+  let response = { data: { finalData: [] }, headerData: { finalData: [] } };
+  readExcelFile(0, response);
+});
+
+function readExcelFile(fileIndex, response) {
+  let fileReader = new FileReader();
+  fileReader.readAsBinaryString(selectedFiles[fileIndex]);
+  fileReader.onload = (event) => {
+    let excelData = event.target.result;
+    let workbook = XLSX.read(excelData, { type: "binary" });
+    response.data[fileIndex + 1] = [];
+    response.headerData[fileIndex + 1] = [];
+    workbook.SheetNames.forEach((sheet, sheetIndex) => {
+      let rowObject = XLSX.utils.sheet_to_json(workbook.Sheets[sheet], {
+        raw: false,
+      });
+      if (rowObject && rowObject.length > 0) {
+        response.headerData[fileIndex + 1] = Object.keys(rowObject[0]);
+        response.headerData.finalData = [
+          ...new Set([
+            ...response.headerData.finalData,
+            ...Object.keys(rowObject[0]),
+          ]),
+        ];
+      }
+      response.data[fileIndex + 1] =
+        response.data[fileIndex + 1].concat(rowObject);
+      response.data.finalData = response.data.finalData.concat(rowObject);
+      if (workbook.SheetNames.length - 1 == sheetIndex) {
+        if (selectedFiles.length - 1 == fileIndex) {
+          exportAsExcelFile(response.data, response.headerData, "Merged.xlsx");
+          console.log(response);
+        } else {
+          readExcelFile(++fileIndex, response);
+        }
+      }
+    });
+  };
+}
+
+function exportAsExcelFile(json, headerName, excelFileName) {
+  let objectMaxLength = [];
+  const workSheet = {};
+  for (const key in json) {
+    Object.keys(headerName[key]).map((headerKey) => {
+      let value =
+        headerName[key][headerKey] === undefined || headerName[key][headerKey] === null
+          ? ""
+          : headerName[key][headerKey];
+      objectMaxLength[headerKey] = value.length + 3;
+    });
+    workSheet[key] = XLSX.utils.json_to_sheet(json[key], {
+      header: headerName[key],
+    });
+    json[key].map((arr) => {
+      Object.keys(headerName[key]).map((headerKey) => {
+        let value =
+          arr[headerName[key][headerKey]] === undefined || arr[headerName[key][headerKey]] === null
+            ? ""
+            : arr[headerName[key][headerKey]];
+        objectMaxLength[headerKey] = Math.max(
+          objectMaxLength[headerKey] || 0,
+          value.toString().length
+        );
+      });
+    });
+    workSheet[key]["!cols"] = objectMaxLength.map((width) => {
+      return {
+        width,
+      };
+    });
+  }
+  const workbook = { Sheets: workSheet, SheetNames: Object.keys(json) };
+
+  const excelBuffer =
+    "data:@file/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64," +
+    XLSX.write(workbook, { bookType: "xlsx", type: "base64" });
+
+  const downloadLink = document.createElement("a");
+  downloadLink.href = excelBuffer;
+  downloadLink.download = excelFileName;
+  downloadLink.click();
+}
